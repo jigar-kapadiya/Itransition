c4fa5b38ade21b63a0d4d9f940ae90c815a9d751
jigar-kapadiya <jigarkapadiya6446+github@gmail.com>
2025-10-09 18:55:36 +0530

    BE | Pagination updates | companies func

--- backend/src/services/drug.service.ts
+++ backend/src/services/drug.service.ts
@@ -1,10 +1,18 @@
 import prisma from "../db/prisma";
 import { Result } from "../types/result";
 
+type Drug = {
+    id: string;
+    code: string;
+    name: string;
+    company: string;
+    launchDate: string;
+}
+
 export class DrugsService {
     constructor() { }
 
-    async getAll(params: { page: number; limit: number; company?: string; }): Promise<Result<any[]>> {
+    async getAll(params: { page: number; limit: number; company?: string; }): Promise<Result<{ rows: Drug[], total: number }>> {
         const { company, limit, page } = params;
         const drugs = await prisma.drug.findMany({
             where: company ? { company } : undefined,
@@ -22,8 +30,22 @@ export class DrugsService {
             launchDate: d.launchDate.toISOString(),
         }));
 
+        const total = await prisma.drug.count({ where: company ? { company } : undefined, })
         return {
-            data: result,
+            data: { rows: result, total }
         }
     }
+
+    async getAllCompanies(): Promise<Result<string[]>> {
+        const companies = await prisma.drug.findMany({
+            distinct: "company",
+            select: {
+                company: true,
+            }
+        });
+
+        return { data: companies.map((j) => j.company), };
+    }
+
+
 }
